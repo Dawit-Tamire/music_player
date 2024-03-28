d082961db3aa868f15d2cfc0a8072159e9bc9392
Dawit-Tamire <101452959+Dawit-Tamire@users.noreply.github.com>
2024-03-28 05:24:49 +0000

    pie chart

--- music_web/src/component/Statistics.tsx
+++ music_web/src/component/Statistics.tsx
@@ -4,16 +4,33 @@ import { css } from '@emotion/react';
 import { Flex, Box, Text } from 'rebass';
 import { RootState } from '../root/config.store';
 import { songActions } from '../slices/songSclice';
+import type { Song } from '../types/types';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import styled from '@emotion/styled';
 
 
+const Container = styled.div`
+    width: "1350px", 
+    height: "738px", 
+    backgroundColor: "lightgray"
+`;
+
+const PieContainer = styled.div`
+    margin-left: 10px
+`;
 
 const SongStatistics: React.FC = () => {
   
   const dispatch = useDispatch();
-
   const { totalNumberOfSongs, totalNumberOfGenres, totalNumberOfAlbums, totalNumberOfArtists,
-            totalNumberOfSongsAndAlbums, totalNumberOfSongsInGenre, totalNumberOfSongsInAlbum } 
-            = useSelector((state: RootState) => state.song);
+    totalNumberOfSongsAndAlbums, totalNumberOfSongsInGenre, totalNumberOfSongsInAlbum } 
+    = useSelector((state: RootState) => state.song);
+    
+  const [data, setData] = useState<any>(null)
+
+  ChartJS.register(ArcElement, Tooltip, Legend);
+  
 
 
   useEffect(() => {
@@ -32,36 +49,68 @@ const SongStatistics: React.FC = () => {
 
   }, [totalNumberOfSongs]);
 
+  const dynamicColors = () => {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    return `rgb(${r},${g},${b})`;
+  };
+
+  const songsAndAlbumsColorArray = totalNumberOfSongsAndAlbums.map(() => dynamicColors());
+  const songsInGenreColorArray = totalNumberOfSongsInGenre.map(() => dynamicColors());
+  const songsInAlbumColorArray = totalNumberOfSongsInAlbum.map(() => dynamicColors());
+
 
   return (
-    <div style={{width: "1350px", height: "738px", backgroundColor: "lightgray"}}>
+    <Container>
       <h1 style={{marginLeft: "50px"}}>Statistics of Songs</h1>
       <Flex flexDirection="row" alignItems="center" mt={70}>
-        <Box width="fit-content" height="fit-content" p={3} bg="white" sx={{ border: '1px solid #ccc' }} mb={4} ml={50}>
+        <Box width="fit-content" height="fit-content" p={3} bg="pink" sx={{ border: '1px solid #ccc' }} mb={4} ml={50}>
           <Text>Total number of songs: {totalNumberOfSongs.totalSongs}</Text>
           <Text>Total number of artists: {totalNumberOfGenres.totalGenres}</Text>
           <Text>Total number of albums: {totalNumberOfAlbums.totalAlbums}</Text>
           <Text>Total number of genres: {totalNumberOfArtists.totalArtists}</Text>
         </Box>
-        <Box width="fit-content" height="fit-content" p={3} bg="white" sx={{ border: '1px solid #ccc' }} mb={4} ml={200} mt={50}>
-          { totalNumberOfSongsInGenre.map((songsInGenre: any)  => (
-            <Text>Number of songs in {songsInGenre._id}: {songsInGenre.count}</Text>
-          ))}
-        </Box>
       </Flex>
-      <Flex flexDirection="row" alignItems="center" mt={10}>
-        <Box width="fit-content" height="fit-content" p={3} bg="white" sx={{ border: '1px solid #ccc' }} mb={4} ml={50}>
-          { totalNumberOfSongsAndAlbums.map((songsAndAlbums: any)  => (
-            <Text>{songsAndAlbums._id} has: {songsAndAlbums.totalSongs} songs and {songsAndAlbums.albums.length} albums</Text>
-          ))}
-        </Box>
-        <Box width="fit-content" height="fit-content" p={3} bg="white" sx={{ border: '1px solid #ccc' }} mb={4} ml={200}>
-          { totalNumberOfSongsInAlbum.map((songsInAlbums: any)  => (
-            <Text>Number of songs in {songsInAlbums._id} album: {songsInAlbums.totalSongs}</Text>
-          ))}
-        </Box>
-      </Flex>
-    </div>
+      <Flex flexDirection="row">
+        <PieContainer>
+          <Pie data={{
+            labels: totalNumberOfSongsAndAlbums.map((songsAndAlbums: any)=>{ return songsAndAlbums._id }),
+            datasets: [
+              {
+                data: totalNumberOfSongsAndAlbums.map((songsAndAlbums: any)=>{ return songsAndAlbums.totalSongs }),
+                backgroundColor: songsAndAlbumsColorArray,
+                hoverBackgroundColor: songsAndAlbumsColorArray,
+              }],
+            }} />
+            <p>Figure 1: Total Number Of Songs And Albums</p>
+        </PieContainer>
+        <PieContainer>
+          <Pie data={{
+            labels: totalNumberOfSongsInGenre.map((songsInGenre: any)=>{ return songsInGenre._id }),
+            datasets: [
+              {
+                data: totalNumberOfSongsInGenre.map((songsInGenre: any)=>{ return songsInGenre.count }),
+                backgroundColor: songsInGenreColorArray,
+                hoverBackgroundColor: songsInGenreColorArray,
+              }],
+          }} />
+          <p>Figure 2: Total Number Of Songs In Genre</p>
+        </PieContainer>
+        <PieContainer>
+          <Pie data={{
+            labels: totalNumberOfSongsInAlbum.map((songsInAlbum: any)=>{ return songsInAlbum._id }),
+            datasets: [
+              {
+                data: totalNumberOfSongsInAlbum.map((songsInAlbum: any)=>{ return songsInAlbum.totalSongs }),
+                backgroundColor: songsInGenreColorArray,
+                hoverBackgroundColor: songsInGenreColorArray,
+              }],
+          }} />
+          <p>Figure 3: Total Number Of Songs In Album</p>
+        </PieContainer>
+      </Flex>       
+    </Container>
   );
 };
 
